26c218f40dc7fcd5ec73224b5a8ef2c4e6038b1a
tndls0720 <tndls154154@gmail.com>
2024-11-22 15:15:16 +0900

    💄 기본 요구 사항 수정
    React 11월 22일

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="flex justify-between items-center px-[20px] py-[10px] bg-black">
+      <h2 className="font-bold text-2xl">OZ코딩스쿨</h2>
+      <ul className="flex justify-center items-center gap-10" >
+        <li className="text-[13px] list-none font-normal">로그인</li>
+        <li className="text-[13px] list-none font-normal">회원가입</li>
+        <li className="text-[13px] list-none font-normal">내클래스</li>
       </ul>
     </header>
   );
